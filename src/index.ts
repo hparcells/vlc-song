6c388d30700adfe4b9c396e21b87d673fc0c167d
Hunter Parcells <hparcells@icloud.com>
2020-07-19 11:59:50 -0400

    Password arg.

--- src/index.ts
+++ src/index.ts
@@ -6,30 +6,36 @@ import xml2js from 'xml2js';
 import getDataFromMeta from './util/get-data-from-meta';
 
 const argv = yargs.options({
-  port: {
+  password: {
     alias: 'p',
+    description: 'Password of the VLC interface.',
+    required: true
+  },
+  port: {
     default: 8080,
     description: 'Port of the VLC interface.'
   }
 }).argv;
 
-axios.get(`http://:password@localhost:${argv.port}/requests/status.xml`).then((response) => {
-  xml2js.parseString(response.data, (error, result) => {
-    if (error) {
-      console.log('An error occurred when parsing the XML data.');
-      return;
-    }
+axios
+  .get(`http://:${argv.password}@localhost:${argv.port}/requests/status.xml`)
+  .then((response) => {
+    xml2js.parseString(response.data, (error, result) => {
+      if (error) {
+        console.log('An error occurred when parsing the XML data.');
+        return;
+      }
 
-    const meta = result.root.information[0].category[0].info;
+      const meta = result.root.information[0].category[0].info;
 
-    const artist = getDataFromMeta(meta, 'artist');
-    const title = getDataFromMeta(meta, 'title');
-    const filename = getDataFromMeta(meta, 'filename');
+      const artist = getDataFromMeta(meta, 'artist');
+      const title = getDataFromMeta(meta, 'title');
+      const filename = getDataFromMeta(meta, 'filename');
 
-    if (title) {
-      console.log(`${artist ? `${artist} - ` : ''}${title}`);
-      return;
-    }
-    console.log(filename);
+      if (title) {
+        console.log(`${artist ? `${artist} - ` : ''}${title}`);
+        return;
+      }
+      console.log(filename);
+    });
   });
-});
